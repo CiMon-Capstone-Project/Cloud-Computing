f85c2e5b39125875c9715934ae38b728435cb86c
Candika <candikadwihandaru@mail.ugm.ac.id>
2024-12-06 19:02:03 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Cimon API server!🚀");
+  res.send("Welcome to Cimon API server!🚀🚀");
 });
 app.use("/api",router)
 
@@ -25,5 +25,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}. whoooshh 🚀🚀`);
+    console.log(`Server is running on PORT ${PORT}. whoooshh🚀`);
 });
