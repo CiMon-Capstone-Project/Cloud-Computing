cd506ec4dbec2cf776bb522995f0f9fec009d720
Candika <candikadwihandaru@mail.ugm.ac.id>
2024-12-11 18:42:15 +0700

    fix cicd

--- src/index.js
+++ src/index.js
@@ -25,5 +25,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}. whoooshh🚀🚀`);
+    console.log(`Server is running on PORT ${PORT}. whoooshh🚀`);
 });
